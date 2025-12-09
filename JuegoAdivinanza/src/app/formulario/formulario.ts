f5c0c5da1a7fe213e5ac7f5a6b1e12142dde93f7
jsarasua <jsarasua@birt.eus>
2025-12-09 18:38:44 +0100

    formularios funcionando

--- JuegoAdivinanza/src/app/formulario/formulario.ts
+++ JuegoAdivinanza/src/app/formulario/formulario.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './formulario.html',
   styleUrl: './formulario.css',
 })
